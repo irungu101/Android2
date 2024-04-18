77b00eac57b6e69f5a01dfba897e54675cd05da2
irungu101 <158286774+irungu101@users.noreply.github.com>
2024-04-18 16:08:43 +0300

    Add files via upload

--- app/src/main/java/com/naomi/android101/ui/theme/screens/login/loginScreen.kt
+++ app/src/main/java/com/naomi/android101/ui/theme/screens/login/loginScreen.kt
@@ -1,30 +1,77 @@
 package com.naomi.android101.ui.theme.screens.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.naomi.android101.navigation.ROUTE_REGISTER
 
 @Composable
 fun LoginScreen(navController: NavController){
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Please Login Here")
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = {navController.navigate(ROUTE_REGISTER) }) {
-            Text(text = "Go to Register Screen")
+//    var email by remember { mutableStateOf(TextFieldValue(""))}
+//    var pass by remember { mutableStateOf(TextFieldValue("")) }
+//    var context = LocalContext.current
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.DarkGray),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(text = "Please Login Here",
+            color = Color.Blue,
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(value = "",
+            onValueChange = {},
+            label = { Text(text = "Enter Your Email")},
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp))
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Login")
         }
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Don't have an account? Click to Register")
+        }
+
+
     }
+
+
+
+
 }
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
